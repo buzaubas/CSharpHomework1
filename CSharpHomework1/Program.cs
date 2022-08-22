087abe2fdcd09c8537aedb94febfa2bd7cecb430
Динмухаммед Бексултанов <beksultanov_d0404@fmalm.nis.edu.kz>
2022-08-22 13:06:46 +0600

    Added two tasks

--- CSharpHomework1/Program.cs
+++ CSharpHomework1/Program.cs
@@ -10,8 +10,22 @@ namespace CSharpHomework1
     {
         static void Main(string[] args)
         {
-            Task1();
-            Task2();
+            string userName;
+            Console.WriteLine("Enter your name and surname->");
+            userName = Console.ReadLine();
+            Console.WriteLine($"Greeting you - {userName}");
+
+            Console.Write('\n');
+
+            int numberToSum1 = new int();
+            int numberToSum2 = new int();
+            Console.WriteLine("Enter first number->");
+            numberToSum1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter second number->");
+            numberToSum2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"Sum of two numbers: {numberToSum1 + numberToSum2}");
+            Console.ReadKey();
+
         }
     }
 }
